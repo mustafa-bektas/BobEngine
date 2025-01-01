f65f002eb25691ecada76b7d20b8d8f2e8e50432
mustafa-bektas <151761340+mustafa-bektas@users.noreply.github.com>
2025-01-01 06:02:12 +0300

    created a simple game loop with event polling and some basic sprite rendering

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,130 @@
-#include <SFML/Graphics.hpp>
+#include <iostream>
+#include <SFMl/Graphics.hpp>
+
+sf::RectangleShape createRectangle(sf::Vector2f size, sf::Vector2f position, sf::Color color);
+sf::CircleShape createCircle(float radius, sf::Vector2f position, sf::Color color);
+sf::Text createText(const sf::Font& font, const sf::String& string, int charSize, sf::Color color, sf::Vector2f position);
+void detectAttack(const std::optional<sf::Event> &event);
+void detectMouseClick(const std::optional<sf::Event> &event);
+void detectContinuousMovement();
 
 int main()
 {
-    auto window = sf::RenderWindow(sf::VideoMode({1920u, 1080u}), "CMake SFML Project");
-    window.setFramerateLimit(144);
+    // create the window
+    sf::RenderWindow window(sf::VideoMode({800,600}), "My Window");
+
+    const sf::Font font("../../assets/arial.ttf");
+
+    const auto rectangle = createRectangle({250, 250}, {35, 100}, sf::Color::Red);
+    const auto circle = createCircle(75, {500, 400}, sf::Color::Blue);
+    const auto text = createText(font, "Hello World!", 24, sf::Color::White, {400, 300});
 
     while (window.isOpen())
     {
+        detectContinuousMovement();
+
         while (const std::optional event = window.pollEvent())
         {
             if (event->is<sf::Event::Closed>())
             {
                 window.close();
             }
-        }
 
-        window.clear();
-        window.display();
+            detectAttack(event);
+            detectMouseClick(event);
+
+            window.clear(sf::Color::Black);
+
+            // draw everything here
+            // window.draw(...)
+            window.draw(rectangle);
+            window.draw(circle);
+            window.draw(text);
+
+            //end the current frame
+            window.display();
+        }
     }
+}
+
+void detectAttack(const std::optional<sf::Event> &event)
+{
+    if (const auto* keyPressed = event->getIf<sf::Event::KeyPressed>())
+    {
+        if (keyPressed->code == sf::Keyboard::Key::Space)
+        {
+            std::cout << "Space key pressed (ATTACK)" << std::endl;
+        }
+    }
+}
+
+void detectMouseClick(const std::optional<sf::Event> &event)
+{
+    if (const auto* mouseButtonPressed = event->getIf<sf::Event::MouseButtonPressed>())
+    {
+        if (mouseButtonPressed->button == sf::Mouse::Button::Left)
+        {
+            std::cout << "Left mouse button pressed" << std::endl;
+            std::cout << "Mouse coordinates at the point of left click: " << mouseButtonPressed->position.x << "," <<
+                mouseButtonPressed->position.y << std::endl;
+        } else if (mouseButtonPressed->button == sf::Mouse::Button::Right)
+        {
+            std::cout << "Right mouse button pressed" << std::endl;
+            std::cout << "Mouse coordinates at the point of right click: " << mouseButtonPressed->position.x << "," <<
+                mouseButtonPressed->position.y << std::endl;
+        }
+    }
+}
+
+void detectContinuousMovement()
+{
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::W))
+    {
+        std::cout << "Moving up" << std::endl;
+    }
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::S))
+    {
+        std::cout << "Moving down" << std::endl;
+    }
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::A))
+    {
+        std::cout << "Moving left" << std::endl;
+    }
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::D))
+    {
+        std::cout << "Moving right" << std::endl;
+    }
+}
+
+sf::RectangleShape createRectangle(const sf::Vector2f size, const sf::Vector2f position, const sf::Color color)
+{
+    sf::RectangleShape rectangle {};
+    rectangle.setSize(size);
+    rectangle.setPosition(position);
+    rectangle.setFillColor(color);
+
+    return rectangle;
+}
+
+sf::CircleShape createCircle(const float radius, const sf::Vector2f position, const sf::Color color)
+{
+    sf::CircleShape circle {};
+    circle.setRadius(radius);
+    circle.setPosition(position);
+    circle.setFillColor(color);
+
+    return circle;
+}
+
+sf::Text createText(const sf::Font& font, const sf::String& string, const int charSize, const sf::Color color, const sf::Vector2f position)
+{
+    sf::Text text(font);
+
+    text.setString(string);
+    text.setCharacterSize(charSize);
+    text.setFillColor(color);
+    text.setStyle(sf::Text::Bold | sf::Text::Underlined);
+    text.setPosition(position);
+
+    return text;
 }
